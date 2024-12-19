3e23a492a37f285d3d4e9b1b45f4c95543799480
fannybdt <132360390+fannybdt@users.noreply.github.com>
2024-12-20 00:37:23 +0100

    simple for in GPU

--- project_info0939/Open_MP_MPI/shallow.c
+++ project_info0939/Open_MP_MPI/shallow.c
@@ -715,7 +715,6 @@ int main(int argc, char **argv)
       }
     }
     else {
-      // TODO: add other sources
       printf("Error: Unknown source type %d\n", param.source_type);
       exit(0);
     }
